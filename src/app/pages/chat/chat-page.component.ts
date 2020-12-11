bc96b9264db5d16c2c3e61ce1dee63a40d37f6c3
Oleksandr <61159320+ZIMC-AYZI@users.noreply.github.com>
2020-12-11 19:05:06 +0200

    timer

--- src/app/pages/chat/chat-page.component.ts
+++ src/app/pages/chat/chat-page.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Self } from '@angular/core';
 
 import { FirestoreUsersService } from '../../core/services/firestore-users.service';
-import {MessagesService} from '../../core/services/messages.service';
-import { Observable } from 'rxjs';
-import {IUserDetail} from '../../interfaces/i-user';
-import {IMessage} from '../../interfaces/i-message';
+import { MessagesService } from '../../core/services/messages.service';
+import { Observable, of, Subscription, timer } from 'rxjs';
+import { IUserDetail } from '../../interfaces/i-user';
+import { IMessage } from '../../interfaces/i-message';
+import { AuthService } from '../../core/services/auth.service';
+import firebase from 'firebase';
+import { concatMap, map, mergeMap, switchMap, take, takeUntil, tap } from 'rxjs/operators';
+import { NgOnDestroy } from '../../core/services/ng-on-destroy.service';
 
 @Component({
   selector: 'app-chat-page',
   templateUrl: './chat-page.component.html',
-  styleUrls: ['./chat-page.component.scss']
+  styleUrls: ['./chat-page.component.scss'],
+  providers: [NgOnDestroy]
 })
 export class ChatPageComponent implements OnInit {
   public users$: Observable<IUserDetail[]>;
   public messages$: Observable<IMessage[]>;
+  public authUser$: Observable<firebase.User>;
 
   constructor(
+    @Self() private ngOnDestroy$: NgOnDestroy,
     private firestoreUsersService: FirestoreUsersService,
-    private messagesService: MessagesService
-  ) { }
+    private messagesService: MessagesService,
+    private authService: AuthService
+  ) {
+  }
 
   ngOnInit(): void {
     this.fetchUsers();
     this.fetchMessages();
+    this.fetchAuthUser();
   }
 
   private fetchUsers(): void {
@@ -31,5 +41,21 @@ export class ChatPageComponent implements OnInit {
 
   private fetchMessages(): void {
     this.messages$ = this.messagesService.getMessagesList('92');
+    this.scrollMessages();
+  }
+
+  private fetchAuthUser(): void {
+    this.authUser$ = this.authService.getAuthUser$();
+  }
+
+  public scrollMessages(): void {
+    timer(1000)
+      .pipe(
+        takeUntil(this.ngOnDestroy$),
+        tap(() => {
+          const box = document.querySelector('.userMessage-wrapper');
+          box.scrollTop = box.scrollHeight;
+        })
+      ).subscribe();
   }
 }
